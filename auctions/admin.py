42efb04806470af90192354a4cfbd1b188680436
achref.abdennebi <achref.abdennebi.ext@suez.com>
2020-08-23 19:44:51 +0200

    feat: init commerce repo

---
+++ auctions/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Auction, Comment, Bid
+# Register your models here.
+
+admin.site.register(Auction)
+admin.site.register(Bid)
+admin.site.register(Comment)
---
+++ auctions/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=250)
+    price = models.FloatField()
+    image_url = models.CharField(max_length=250)
+    created_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.title} ({self.price})"
+
+class Bid(models.Model):
+    value = models.FloatField()
+    created_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.value} {self.created_date}"
+
+class Comment(models.Model):
+    content = models.CharField
+    created_date  = models.DateTimeField()
+
+
